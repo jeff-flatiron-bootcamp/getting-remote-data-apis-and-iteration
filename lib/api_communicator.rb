a1538f452ed4df96bc1888ee8d9c090a8a8c5b49
jeff_simon <jeffsbillinfo@gmail.com>
2020-03-23 13:41:00 -0700

    added REST Reponse

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,10 +3,11 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-
+  response_hash = json_parse(rest_request('http://www.swapi.co/api/people/'))
+  character_films = find_films_by_character(response_hash, character_name)
+  film_data_res = map_film_data_res(character_films)
+  film_data_json = map_film_data_json(film_data_res)
+  
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -15,15 +16,38 @@ def get_character_movies_from_api(character_name)
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  #  of movies by title. Have a play around with the puts with other info about a given film.  
+end
+
+def map_film_data_res(character_films)  
+  value = character_films["films"].sort.map{|film| rest_request(film)}
+end
+
+def map_film_data_json(film_data_res)
+  film_data_res.map{|response| JSON.parse(response)}
+end
+
+def rest_request(url)
+  RestClient.get(url)
+end
+
+def json_parse(res)
+    JSON.parse(res)
+end
+
+def find_films_by_character(results, character_name)
+  results["results"].find{|character| character["name"] == character_name}  
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  # some iteration magic and puts out the movies in a nice list  
+  titles = films.map{|data| data["title"]}
+  titles.each_with_index{|title, index| puts "#{index+1} #{title}"}
 end
 
 def show_character_movies(character)
-  films = get_character_movies_from_api(character)
+
+  films = get_character_movies_from_api(character)  
   print_movies(films)
 end
 
